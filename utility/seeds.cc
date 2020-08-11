1293faff1cef56c4c7138ed04e4923d93618aad7
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-11 17:29:47 -0400

    add methods and refactor seeds file

---
+++ utility/seeds.cc
@@ -0,0 +1,109 @@
+#include "seeds.h"
+
+void traveseOwnable() { // for testing
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        for (int j = 0; j < ATTRIBUTE_BUILDING_SIZE; j++) {
+            std::cout << OWNABLE[i][j] << " " ;
+        }
+        std::cout << std::endl;
+    }
+}
+
+// squareName should be valid ownable building name
+std::string monoBlockOfProp(std::string squareName) {
+    std::string result = "";
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result = OWNABLE[i][MONOBLOCK_INDEX];
+        }
+    }
+
+    return result;
+}
+
+// squareName should be valid ownable building name
+int costToBuyProp(std::string squareName) {
+    int result = 0;
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result += std::stoi(OWNABLE[i][BUY_COST_INDEX]);
+        }
+    }
+
+    return result;
+}
+
+// squareName should be valid ownable building name
+int costToImprProp(std::string squareName) {
+    int result = 0;
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result += std::stoi(OWNABLE[i][IMPR_COST_INDEX]);
+        }
+    }
+
+    return result;
+}
+
+// squareName should be valid ownable building name
+int costToMortProp(std::string squareName) {
+    int result = 0;
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result += std::stoi(OWNABLE[i][BUY_COST_INDEX]) * 0.5;
+        }
+    }
+
+    return result;
+}
+
+// squareName should be valid ownable building name
+int costToUnmortProp(std::string squareName) {
+    int result = 0;
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result += std::stoi(OWNABLE[i][BUY_COST_INDEX]) * 0.6;
+        }
+    }
+
+    return result;
+}
+
+// squareName should be valid ownable building name
+int costToPayUnimprProp(std::string squareName) {
+    int result = 0;
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result += std::stoi(OWNABLE[i][IMPR_PRICE_0_INDEX]);
+        }
+    }
+
+    return result;
+}
+
+// squareName should be valid ownable building name
+int costToPayImprProp(std::string squareName, int imprLevel) {
+    int result = 0;
+    int levelIndex = 0;
+
+    if (imprLevel = 0) levelIndex = IMPR_PRICE_0_INDEX;
+    if (imprLevel = 1) levelIndex = IMPR_PRICE_1_INDEX;
+    if (imprLevel = 2) levelIndex = IMPR_PRICE_2_INDEX;
+    if (imprLevel = 3) levelIndex = IMPR_PRICE_3_INDEX;
+    if (imprLevel = 4) levelIndex = IMPR_PRICE_4_INDEX;
+    if (imprLevel = 5) levelIndex = IMPR_PRICE_5_INDEX;
+
+    for (int i = 0; i < OWNABLE_SIZE; i++) {
+        if (OWNABLE[i][NAME_INDEX] == squareName) {
+            result += std::stoi(OWNABLE[i][levelIndex]);
+        }
+    }
+
+    return result;
+}
--- utility/seeds.h
+++ utility/seeds.h
@@ -3,6 +3,7 @@
 #include <string>
 
 void traveseOwnable();
+std::string monoBlockOfProp(std::string squareName);
 int costToBuyProp(std::string squareName);
 int costToImprProp(std::string squareName);
 int costToMortProp(std::string squareName);
@@ -74,99 +75,3 @@ const int GYM_SIZE = GYM.size();
 const std::vector<std::vector<std::string>> OWNABLE = {AL, ML, ECH, PAS, HH, RCH, DWE, CPH, LHI, BMH, OPT, EV1, EV2, EV3, PHYS, B1, B2, EIT, ESC, C2, MC, DC,
                                                        MKV, UWP, V1, RES, PAC, CIF};
 const int OWNABLE_SIZE = OWNABLE.size();
-
-
-void traveseOwnable() { // for testing
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        for (int j = 0; j < ATTRIBUTE_BUILDING_SIZE; j++) {
-            std::cout << OWNABLE[i][j] << " " ;
-        }
-        std::cout << std::endl;
-    }
-}
-
-// squareName should be valid ownable building name
-int costToBuyProp(std::string squareName) {
-    int result = 0;
-
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        if (OWNABLE[i][NAME_INDEX] == squareName) {
-            result += std::stoi(OWNABLE[i][BUY_COST_INDEX]);
-        }
-    }
-
-    return result;
-}
-
-// squareName should be valid ownable building name
-int costToImprProp(std::string squareName) {
-    int result = 0;
-
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        if (OWNABLE[i][NAME_INDEX] == squareName) {
-            result += std::stoi(OWNABLE[i][IMPR_COST_INDEX]);
-        }
-    }
-
-    return result;
-}
-
-// squareName should be valid ownable building name
-int costToMortProp(std::string squareName) {
-    int result = 0;
-
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        if (OWNABLE[i][NAME_INDEX] == squareName) {
-            result += std::stoi(OWNABLE[i][BUY_COST_INDEX]) * 0.5;
-        }
-    }
-
-    return result;
-}
-
-// squareName should be valid ownable building name
-int costToUnmortProp(std::string squareName) {
-    int result = 0;
-
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        if (OWNABLE[i][NAME_INDEX] == squareName) {
-            result += std::stoi(OWNABLE[i][BUY_COST_INDEX]) * 0.6;
-        }
-    }
-
-    return result;
-}
-
-// squareName should be valid ownable building name
-int costToPayUnimprProp(std::string squareName) {
-    int result = 0;
-
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        if (OWNABLE[i][NAME_INDEX] == squareName) {
-            result += std::stoi(OWNABLE[i][IMPR_PRICE_0_INDEX]);
-        }
-    }
-
-    return result;
-}
-
-// squareName should be valid ownable building name
-int costToPayImprProp(std::string squareName, int imprLevel) {
-    int result = 0;
-    int levelIndex = 0;
-
-    if (imprLevel = 0) levelIndex = IMPR_PRICE_0_INDEX;
-    if (imprLevel = 1) levelIndex = IMPR_PRICE_1_INDEX;
-    if (imprLevel = 2) levelIndex = IMPR_PRICE_2_INDEX;
-    if (imprLevel = 3) levelIndex = IMPR_PRICE_3_INDEX;
-    if (imprLevel = 4) levelIndex = IMPR_PRICE_4_INDEX;
-    if (imprLevel = 5) levelIndex = IMPR_PRICE_5_INDEX;
-
-    for (int i = 0; i < OWNABLE_SIZE; i++) {
-        if (OWNABLE[i][NAME_INDEX] == squareName) {
-            result += std::stoi(OWNABLE[i][levelIndex]);
-        }
-    }
-
-    return result;
-}
