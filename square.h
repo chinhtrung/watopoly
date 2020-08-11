9ea37788d06349f67fdb96074115235c79e74cdc
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-10 23:40:24 -0400

    update files

--- square.h
+++ square.h
@@ -1,8 +1,16 @@
 #ifndef SQUARE_H
 #define SQUARE_H
+#include <string>
 
 class Square{
+    int ID;
+    std::string name;
+  public:
     Square();
+
+    void drawSqr();
+    int getID();
+    std::string getName();
 };
 
 #endif
