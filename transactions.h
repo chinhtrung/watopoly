8f79101045725ad4454032d12a929a17c791d4b6
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-12 19:27:47 -0400

    change declaration and add implementation

--- transactions.h
+++ transactions.h
@@ -16,14 +16,13 @@ class Transactions{
     void tradeMforP(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int give, std::shared_ptr<Ownable> receive);
     void tradePforP(std::shared_ptr<Player> from, std::shared_ptr<Player> to, std::shared_ptr<Ownable> give, std::shared_ptr<Ownable> receive);
     void tradePforM(std::shared_ptr<Player> from, std::shared_ptr<Player> to, std::shared_ptr<Ownable> give, int receive);
-    void payRent(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int rent);
-    void payBank(std::shared_ptr<Player> from);
-    void payPlayer(std::shared_ptr<Player> to);
-    void buyProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
-    void sellProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
+    void payPlayer(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int rent);
+    void payBank(std::shared_ptr<Player> from, int amt);
+    void buyProperty(std::string ownableName, std::shared_ptr<Player> buyer);
     void improveProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
     void sellImprove(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
-    void mortgageProper(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own); 
+    void mortgageProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
+    void unmortgageProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own); 
 };
 
 #endif
