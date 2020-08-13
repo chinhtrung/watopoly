0a24b4eae2f5ef90f9b7bb69c209d05f2ac28abb
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-13 17:50:51 -0400

    change transaction method to static

--- transactions.h
+++ transactions.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <iostream>
 #include <vector>
+#include "utility/seeds.h"
 #include "player.h"
 #include "ownable.h"
 
@@ -13,16 +14,17 @@ class Transactions{
   public:
     Transactions();
     
-    void tradeMforP(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int give, std::shared_ptr<Ownable> receive);
-    void tradePforP(std::shared_ptr<Player> from, std::shared_ptr<Player> to, std::shared_ptr<Ownable> give, std::shared_ptr<Ownable> receive);
-    void tradePforM(std::shared_ptr<Player> from, std::shared_ptr<Player> to, std::shared_ptr<Ownable> give, int receive);
-    void payPlayer(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int rent);
-    void payBank(std::shared_ptr<Player> from, int amt);
-    void buyProperty(std::string ownableName, std::shared_ptr<Player> buyer);
-    void improveProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
-    void sellImprove(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
-    void mortgageProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
-    void unmortgageProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own); 
+    static void tradeMforP(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int give, std::shared_ptr<Ownable> receive);
+    static void tradePforP(std::shared_ptr<Player> from, std::shared_ptr<Player> to, std::shared_ptr<Ownable> give, std::shared_ptr<Ownable> receive);
+    static void tradePforM(std::shared_ptr<Player> from, std::shared_ptr<Player> to, std::shared_ptr<Ownable> give, int receive);
+    static void payPlayer(std::shared_ptr<Player> from, std::shared_ptr<Player> to, int rent);
+    static void payBank(std::shared_ptr<Player> from, int amt);
+    static void buyProperty(std::string ownableName, std::shared_ptr<Player> buyer);
+    static void improveProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
+    static void sellImprove(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
+    static void mortgageProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
+    static void unmortgageProperty(std::shared_ptr<Ownable> prop, std::shared_ptr<Player> own);
+    static void actingAtCurrentPosition(std::shared_ptr<Player> own);
 };
 
 #endif
