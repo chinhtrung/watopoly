0b2ae23b01801b84432cbf9ff14be919ce8672b6
w499wang <w499wang@student.cs.uwaterloo.ca>
2020-08-12 21:09:53 -0400

    separated header and implementation

---
+++ positionMap.cc
@@ -0,0 +1,17 @@
+#include "positionMap.h"
+
+
+// return the first index of nameSquare in the MAP_GAME
+// nameSquare should be a valid name
+int indexOfSquare(std::string nameSquare) {
+    int indexResult = 0;
+
+    for (int i = 0; i < MAP_GAME_SIZE; i++) {
+        if (nameSquare == MAP_GAME[i]) {
+            indexResult = i;
+            break;
+        }
+    }
+
+    return indexResult;
+}
