bf307a1cc737c1be1cd9f001f2a1f0b4c648ea50
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-14 10:53:56 -0400

    fix space and indentation

--- Unownable.cc
+++ Unownable.cc
@@ -1,6 +1,1 @@
 #include "Unownable.h"
-using namespace std;
-
-
-Unownable::Unownable() {
-}
--- Unownable.h
+++ Unownable.h
@@ -1,16 +1,12 @@
 #ifndef UNOWNABLE_H
 #define UNOWNABLE_H
-
-
 #include <memory>
 #include "player.h"
-
 
 class Unownable: public Square {
 	public:
 		Unownable();
 		virtual void actionAtIndex(std::shared_ptr<Player> p) = 0;
 };
-
 
 #endif
