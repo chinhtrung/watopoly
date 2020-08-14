a2fa9c04517a8d860a98a95b429233628a5c12ed
w499wang <w499wang@student.cs.uwaterloo.ca>
2020-08-13 22:22:50 -0400

    added testing for RNG

--- MonetaryServices.cc
+++ MonetaryServices.cc
@@ -49,6 +49,36 @@ int MonetaryServices::needlesHall() {
 }
 
 
+int MonetaryServices::needlesHallTest() {
+
+	srand(103);
+	int timsRoll = rand() % (MAX_TIMS_ROLL - MIN_TIMS_ROLL + 1) 
+	       + MIN_TIMS_ROLL;
+	if (timsRoll == SECRET_NUM) {
+		return 0;
+	}
+	int spin = rand() % (MAX - MIN + 1) + MIN;
+	if (spin == 1) {
+		return -200;
+	} else if (spin >= 2 && spin <= 3) {
+		return -100;
+	} else if (spin >= 4 && spin <= 6) {
+		return -50;
+	} else if (spin >= 7 && spin <= 12) {
+		return 25;
+	} else if (spin >= 13 && spin <= 15) {
+		return 50;
+	} else if (spin >= 16 && spin <= 17) {
+		return 100;
+	} else if (spin == 18) {
+		return 200;
+	} else {
+		throw (out_of_range);
+	}
+	return 0;
+}
+
+
 int MonetaryServices::collectOSAP() {
 	return 200;
 }
