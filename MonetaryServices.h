c9244162b5aa5f4d72075401b515df895094d9c2
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-15 02:09:24 -0400

    review and merge

--- MonetaryServices.h
+++ MonetaryServices.h
@@ -1,26 +1,20 @@
 #ifndef MONETARY_SERVICES_H
 #define MONETARY_SERVICES_H
-
-
 #include <memory>
 #include <string>
 #include "Unownable.h"
 
-
 class Player;
-
 
 class MonetaryServices: public Unownable{
 	public:
 		MonetaryServices(int ID, std::string name);
 		virtual void actionAtIndex(std::shared_ptr<Player> p) override;
-
 		static int needlesHall();
 		static int needlesHallTest();
 		static int collectOSAP();
 		static int payCoop();
 		static int payTuition(std::shared_ptr<Player> p);
 };
-
 
 #endif
