a5b935b44d3f878bc846812d938d4a07c0a94e6d
w499wang <w499wang@student.cs.uwaterloo.ca>
2020-08-14 22:56:52 -0400

    testing fix

--- TimsLine.h
+++ TimsLine.h
@@ -4,13 +4,14 @@
 
 #include <map>
 #include <memory>
+#include <string>
 #include "Unownable.h"
 
 
 class TimsLine: public Unownable {
 	std::map<char, int> captured; // e.g. "A": 2, "B": 0
 	public:
-		TimsLine();
+		TimsLine(int ID, std::string name);
 		virtual void actionAtIndex(std::shared_ptr<Player> p) override;
 		void addRoll(char player);
 		bool isCaptured(char player);
