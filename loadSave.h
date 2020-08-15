015d51cd5e3b9d242b1734e3b54fb198fe188dd1
c622wang <c622wang@student.cs.uwaterloo.ca>
2020-08-15 15:56:36 -0400

    latest version

--- loadSave.h
+++ loadSave.h
@@ -2,19 +2,18 @@
 #define _LOADSAVE_H
 #include <string>
 #include <memory>
-#include "player.h"
-#include "ownable.h"
 
 class Player;
+class Square;
 class Ownable;
 class Academic;
 class Residence;
 class Gym;
-class Square;
+class Transactions;
 
 class LoadSave{
   public:
-    void loadProperty(std::string ownableName, std::shared_ptr<Player> buyer, int imprLevel);
+    static void loadProperty(std::string ownableName, std::shared_ptr<Player> buyer, int imprLevel);
 };
 
 #endif
