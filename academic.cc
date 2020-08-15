51666042e622624cbfb323ee83c96f904dde1310
c622wang <c622wang@student.cs.uwaterloo.ca>
2020-08-14 22:17:44 -0400

    partially implemented academic

--- academic.cc
+++ academic.cc
@@ -2,13 +2,34 @@
 
 Academic::Academic(int ID, std::string name, int costToBuy, 
 		           char owner, std::string monopolyBlock) 
-    : Ownable(ID, name, costToBuy, owner), monopolyBlock{monopolyBlock}
-{}
+    : Ownable(ID, name, costToBuy, owner), monopolyBlock{monopolyBlock} {
+    tuition = costToPayUnimprProp(name);    
+}
 
 int Academic::amountToPay() {
     return tuition;
 }
 
 void Academic::updateTuition() {
-    tuition = costToPayImprProp(this->getName(), this->getPayLevel());
+    if (this->getMortStatus()){
+        tuition = 0;
+    } else if (blockOwned){
+        tuition = costToPayUnimprPropBlock(name);
+    } else if (this->getImprLevel() == 0){
+        tuition = costToPayUnimprProp(name);
+    } else {
+        tuition = costToPayImprProp(name, this->getImprLevel);
+    } 
+}
+
+void Academic::setBlockOwned(bool status){
+    blockOwned = status;
+}
+
+bool Academic::getBlockOwned(){
+    return blockOwned;
+}
+
+std::string Academic::getMonoBlock() {
+    return monopolyBlock;
 }
