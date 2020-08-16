fe0c5cdc9d2656b098ab7c1d598bf82e9f855999
Trung Nguyen <chinhtrung596@gmail.com>
2020-08-15 21:46:07 -0400

    latest push

--- main.cc
+++ main.cc
@@ -459,7 +459,7 @@ int main(int argc, char **argv)
         }
         else if (command == SAVE)
         {
-            cout << "--> " << currActingPlayer->getName() << "is about to save the game <--" << endl;
+            cout << "--> " << currActingPlayer->getName() << " is about to save the game <--" << endl;
             cout << "--> input the name file you want to save" << endl;
             string file;
             cin >> file;
